eebf6c653627b1a08c56b539fb13893a13a746a6
Nick Perry <nperry@williamhill.plc>
2015-10-13 01:20:57 +0100

    pass splunk_user to posix class

--- manifests/forwarder.pp
+++ manifests/forwarder.pp
@@ -106,7 +106,8 @@ class splunk::forwarder (
   # there is non-generic configuration that needs to be declared in addition
   # to the agnostic resources declared here.
   case $::kernel {
-    'Linux': { class { 'splunk::platform::posix': splunkd_port => $splunkd_port, } }
+    'Linux': { class { 'splunk::platform::posix': splunkd_port => $splunkd_port,
+                                                  splunk_user  => $splunk_user } }
     'SunOS': { include splunk::platform::solaris }
     default: { } # no special configuration needed
   }
