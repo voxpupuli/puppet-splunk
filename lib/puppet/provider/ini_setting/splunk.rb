3ed559d8e74e0683c466dd47a8aafaac8ae9b64b
Alexander Fisher <alex@linfratech.co.uk>
2016-09-26 21:31:41 +0100

    rubocop: fix Style/RedundantSelf
    Signed-off-by: Alexander Fisher <alex@linfratech.co.uk>

--- lib/puppet/provider/ini_setting/splunk.rb
+++ lib/puppet/provider/ini_setting/splunk.rb
@@ -9,7 +9,7 @@ Puppet::Type.type(:ini_setting).provide(
 
   def self.file_path
     raise Puppet::Error, 'file_path must be set with splunk_config type before provider can be used' if @file_path.nil?
-    raise Puppet::Error, 'Child provider class does not support a file_name method' unless self.respond_to?(:file_name)
+    raise Puppet::Error, 'Child provider class does not support a file_name method' unless respond_to?(:file_name)
     File.join(@file_path, file_name)
   end
 
