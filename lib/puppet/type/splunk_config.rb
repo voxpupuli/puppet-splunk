038a6679ba059c35eb1530e1015117260ba72918
Alexander Fisher <alex@linfratech.co.uk>
2016-09-26 21:29:23 +0100

    rubocop: fix Style/IfUnlessModifier
    Signed-off-by: Alexander Fisher <alex@linfratech.co.uk>

--- lib/puppet/type/splunk_config.rb
+++ lib/puppet/type/splunk_config.rb
@@ -1,8 +1,6 @@
 # Require all of our types so the class names are resolvable for purging
 Dir[File.dirname(__FILE__) + '/*.rb'].each do |file|
-  unless file == __FILE__
-    require file
-  end
+  require file unless file == __FILE__
 end
 
 Puppet::Type.newtype(:splunk_config) do
