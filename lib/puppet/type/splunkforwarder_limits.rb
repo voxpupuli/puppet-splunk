1511fcdc309da65caf18628ce5bb200db1990724
Nicholas Pearson <npearson@williamhill.co.uk>
2017-07-19 10:54:50 +0100

    Added splunkforwarder_server and splunkforwarder_limits providers/types.

---
+++ lib/puppet/type/splunkforwarder_limits.rb
@@ -0,0 +1,6 @@
+require File.join(File.dirname(__FILE__), '..', '..', 'puppet_x/puppetlabs/splunk/type')
+
+Puppet::Type.newtype(:splunkforwarder_limits) do
+  @doc = 'Manage splunkforwarder limit settings in limits.conf'
+  PuppetX::Puppetlabs::Splunk::Type.clone_type(self)
+end
