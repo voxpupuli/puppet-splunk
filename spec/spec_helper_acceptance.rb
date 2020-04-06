99179f36578fe3c4953b1db75379d29ca0dcc6ff
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-06 23:12:46 +0200

    Use voxpupuli-acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,29 +1,13 @@
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 require 'splunk_data.rb'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+configure_beaker do |host|
+  # Need to stage the Splunk/Splunkforwarder packages here.
 
-RSpec.configure do |c|
-  # Readable test descriptions
-  c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    install_module
-    install_module_dependencies
-
-    # Need to stage the Splunk/Splunkforwarder packages here.
-
-    # The splunk unit file assumes certain cgroups are present, which is not
-    # the case in the testing container(s).  Create cgroups resources here.
-    hosts.each do |host|
-      on(host, '/bin/mkdir -p /sys/fs/cgroup/cpu/system.slice/Splunkd.service')
-      on(host, '/bin/mkdir -p /sys/fs/cgroup/memory/system.slice/Splunkd.service')
-      on(host, '/bin/mkdir -p /sys/fs/cgroup/cpu/system.slice/SplunkForwarder.service')
-      on(host, '/bin/mkdir -p /sys/fs/cgroup/memory/system.slice/SplunkForwarder.service')
-    end
-  end
+  # The splunk unit file assumes certain cgroups are present, which is not
+  # the case in the testing container(s).  Create cgroups resources here.
+  on(host, '/bin/mkdir -p /sys/fs/cgroup/cpu/system.slice/Splunkd.service')
+  on(host, '/bin/mkdir -p /sys/fs/cgroup/memory/system.slice/Splunkd.service')
+  on(host, '/bin/mkdir -p /sys/fs/cgroup/cpu/system.slice/SplunkForwarder.service')
+  on(host, '/bin/mkdir -p /sys/fs/cgroup/memory/system.slice/SplunkForwarder.service')
 end
