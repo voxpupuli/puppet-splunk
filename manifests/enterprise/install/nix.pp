0b5f73e1c059099f211dc52c1a7354f97697b607
Jonas Nicole <jonas.nicole@elca.ch>
2020-08-03 13:48:54 +0200

    add manage_dependencies for external packages

--- manifests/enterprise/install/nix.pp
+++ manifests/enterprise/install/nix.pp
@@ -32,7 +32,7 @@ class splunk::enterprise::install::nix inherits splunk::enterprise::install {
   }
 
   # Required for splunk 7.2.4.2
-  if versioncmp($splunk::enterprise::version, '7.2.4.2') >= 0 {
+  if versioncmp($splunk::enterprise::version, '7.2.4.2') >= 0  and $splunk::forwarder::manage_dependencies {
     ensure_packages(['net-tools'], {
       'ensure' => 'present',
       before   => Package[$splunk::enterprise::enterprise_package_name]
