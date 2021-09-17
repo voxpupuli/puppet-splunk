5778673eee9fa92453fda520cfafe23651abaac1
mvandegarde <michiel@usn.nl>
2021-09-17 14:35:24 +0200

    Safer version check
    This prevents facter from getting stuck in an interactive session

--- lib/facter/splunkforwarder_version.rb
+++ lib/facter/splunkforwarder_version.rb
@@ -4,7 +4,7 @@ Facter.add(:splunkforwarder_version) do
     cmd = if File.exist?('C:/Program Files/SplunkUniversalForwarder/bin/splunk.exe')
             '"C:/Program Files/SplunkUniversalForwarder/bin/splunk.exe" --version'
           elsif File.exist?('/opt/splunkforwarder/bin/splunk')
-            '/opt/splunkforwarder/bin/splunk --version'
+            '/opt/splunkforwarder/bin/splunk --version --accept-license --answer-yes --no-prompt'
           end
     if cmd
       output = Facter::Util::Resolution.exec(cmd)
