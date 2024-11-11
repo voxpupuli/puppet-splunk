48f50657d6500ebd530508181487d3bb4875b4c6
luedderd <74256035+luedderd@users.noreply.github.com>
2024-11-11 13:37:45 -0700

    fix a bunch of broken bits :)

--- manifests/edgeprocessor/config.pp
+++ manifests/edgeprocessor/config.pp
@@ -4,9 +4,7 @@
 #
 class splunk::edgeprocessor::config {
   #Config file with connection details for Splunk Cloud Services environment
-  $scs_config = "groupId: ${splunk::edgeprocessor::scs_group_id}
-    tenant: ${splunk::edgeprocessor::scs_tenant_name}
-    env: ${splunk::edgeprocessor::scs_environment_name}"
+  $scs_config = "groupId: ${splunk::edgeprocessor::scs_group_id}\ntenant: ${splunk::edgeprocessor::scs_tenant_name}\nenv: ${splunk::edgeprocessor::scs_environment_name}"
 
   file { "${splunk::edgeprocessor::splunk_homedir}/etc/config.yaml":
     ensure  => file,
--- manifests/edgeprocessor/install.pp
+++ manifests/edgeprocessor/install.pp
@@ -4,19 +4,24 @@
 #
 class splunk::edgeprocessor::install (
   String[1] $archive_name = regsubst($splunk::edgeprocessor::package_url, '(\\S*\\/)([^\\/]+\\.tar\\.gz$)', '\\2'),
+  Stdlib::UnixPath $extract_path = regsubst($splunk::edgeprocessor::splunk_homedir, '(^.*\\/)[^\\/]*.$','\\1'),
 ) {
   #Download and unpack the Splunk Edge Processor
-  archive { $splunk::edgeprocessor::archive_name:
+  archive { $archive_name:
     path          => "/tmp/${archive_name}",
     source        => $splunk::edgeprocessor::package_url,
     checksum      => $splunk::edgeprocessor::splunk_package_checksum,
     checksum_type => $splunk::edgeprocessor::checksum_type,
     extract       => true,
-    extract_path  => "${splunk::edgeprocessor::splunk_homedir}/..",
+    extract_path  => $extract_path,
     creates       => $splunk::edgeprocessor::splunk_homedir,
     cleanup       => true,
-    user          => $splunk::edgeprocessor::splunk_user,
-    group         => $splunk::edgeprocessor::splunk_user,
+  }
+  file { $splunk::edgeprocessor::splunk_homedir:
+    ensure  => directory,
+    recurse => true,
+    owner   => $splunk::edgeprocessor::splunk_user,
+    group   => $splunk::edgeprocessor::splunk_user,
   }
   file { "${splunk::edgeprocessor::splunk_homedir}/var/log":
     ensure => directory,
