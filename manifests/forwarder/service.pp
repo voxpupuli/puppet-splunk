1f8dee55c6fc57d84cb6148e17f707078aff28b2
Cees-Jan Swart <cees-jan.swart@kpn.com>
2024-12-11 16:27:25 +0100

    Fix rspec error Provider redhat must have features 'delayed_startable'

--- manifests/forwarder/service.pp
+++ manifests/forwarder/service.pp
@@ -4,10 +4,15 @@
 #   sub-classes
 #
 class splunk::forwarder::service {
+  $provider = $facts['os']['family'] ? {
+    'windows' => 'windows',
+    default   => 'redhat',
+  }
   service { $splunk::forwarder::service_name:
     ensure     => $splunk::forwarder::service_ensure,
     enable     => $splunk::forwarder::service_enable,
     hasstatus  => true,
     hasrestart => true,
+    provider   => $provider,
   }
 }
